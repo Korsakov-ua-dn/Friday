d5a15b88e92b88fcc4c78c99cf8df12119e85618
lergnom <lergnom@mail.ru>
2021-09-17 20:30:36 +0300

    added styles && jsx for registration

--- src/features/f2-Registration/r1-UI/Registration.tsx
+++ src/features/f2-Registration/r1-UI/Registration.tsx
@@ -1,9 +1,41 @@
-// import s from './Registration.module.css'
+import s from './Registration.module.css';
 
 export const Registration: React.FC = () => {
     return (
-        <div>
-            Registration...from LerGnom
-        </div>
-    )
-}
+        <>
+            <div className={s.pageWrapper}>
+                <div className={s.pageContainer}>
+                    <div className={s.pageHeader}>
+                        <p className={s.pageLogo}>
+                            <span>SignUp</span> page
+                        </p>
+                    </div>
+                    <div className={s.pageHr}></div>
+                    <form>
+                        <div className={s.formStyle}>
+                            <label className={s.formLabel}>Login: </label>
+                            {/*<SuperInput/>*/}
+                        </div>
+
+                        <div className={s.formStyle}>
+                            <label className={s.formLabel}>Your Password: </label>
+                            {/*<SuperInput changeType="password"/>*/}
+                        </div>
+                        <div className={s.formStyle}>
+                            <label className={s.formLabel}>Repeat Password: </label>
+                            {/*<SuperInput changeType="password"/>*/}
+                        </div>
+
+                        <div className={s.formStyle}>
+                            {/*<SuperButton btnPrimary>*/}
+                            SignUp
+                            {/*</SuperButton>*/}
+                        </div>
+
+                    </form>
+
+                </div>
+            </div>
+        </>
+    );
+};
