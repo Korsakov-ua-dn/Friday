d657db53ac11366c5f566ab91fd8b6c879710bf8
DemaJS <demavictor@mail.ru>
2021-09-20 01:32:12 +0300

    add styles profiles

--- src/features/f5-Profile/p1-UI/Profile.tsx
+++ src/features/f5-Profile/p1-UI/Profile.tsx
@@ -1,9 +1,36 @@
-// import s from './Profile.module.css'
+import React from "react";
+import classes from './Profile.module.css'
+import Button from "../../../common/c2-Button/Button";
 
 export const Profile: React.FC = () => {
+
+
     return (
-        <div>
-            Profile...
-        </div>
+        <>
+            <div className={classes.wrapper}>
+                <div className={classes.avatar}>
+                    <div className={classes.description}>
+                        <div className={classes.ava}>
+                            <img/>
+                        </div>
+                        <span>Petr Ivanov</span>
+                        <span>Front-end developer</span>
+                        <Button>LOG OUT</Button>
+                    </div>
+                    <div className={classes.numbers}>
+
+                    </div>
+                </div>
+                    <div className={classes.cards}>
+                        <div className={classes.input}>
+                            <h2>Packs list</h2>
+                            <input placeholder='Search...'/>
+                        </div>
+                        <div className={classes.list}>
+
+                        </div>
+                </div>
+            </div>
+        </>
     )
 }
