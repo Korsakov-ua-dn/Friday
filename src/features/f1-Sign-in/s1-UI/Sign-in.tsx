ddafa8a1695d6578076d15c570a4ed2fb601fca5
Astaroth <minatosavich@gmail.com>
2021-09-20 18:22:36 +0300

    feat: add add jsx page template

--- src/features/f1-Sign-in/s1-UI/Sign-in.tsx
+++ src/features/f1-Sign-in/s1-UI/Sign-in.tsx
@@ -2,11 +2,15 @@ import React, {useCallback, useState} from "react";
 import s from './Sign-in.module.css'
 import {InputText} from "../../../common/c1-Input/InputText";
 import {InputPassword} from "./Common/InputPassword/InputPassword";
+import {NavLink} from "react-router-dom";
+import Button from "../../../common/c2-Button/Button";
+import Checkbox from "../../../common/c3-Checkbox/Checkbox";
 
 
 export const SignIn: React.FC = () => {
     const [email, setEmail] = useState<string>("")
     const [password, setPassword] = useState<string>("")
+    const [rememberMe, setRememberMe] = useState<boolean>(false)
 
     const onChangeEmail = (text: string) => {
         setEmail(text)
@@ -15,6 +19,11 @@ export const SignIn: React.FC = () => {
     const onChangePassword = (password: string) => {
         setPassword(password)
     }
+
+    const onChangeRememberMe = (checked: boolean) => {
+        setRememberMe(checked)
+    }
+
 
     return (
         <div className={s.wrapper}>
@@ -34,6 +43,17 @@ export const SignIn: React.FC = () => {
                         setError={() => true}
                         label={"Password"}
                     />
+                    <div className={s.checkboxWrapper}>
+                        <Checkbox onChangeChecked={onChangeRememberMe} checked={rememberMe}>Remember me</Checkbox>
+                    </div>
+                </div>
+                <div className={s.forgot}>
+                    <NavLink to={"/forgot"} className={s.link}>Forgot Password?</NavLink>
+                </div>
+                <Button>Login</Button>
+                <div className={s.registration}>
+                    <p>Don't have an account? </p>
+                    <NavLink to={"/registration"} className={s.link}>Sign Up</NavLink>
                 </div>
             </div>
         </div>
