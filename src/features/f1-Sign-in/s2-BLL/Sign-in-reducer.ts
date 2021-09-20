a02d30d0fdfe83ee8853b79dee751f0bbf9b87d1
Astaroth <minatosavich@gmail.com>
2021-09-20 19:13:31 +0300

    refactor: change type in authUserAC

--- src/features/f1-Sign-in/s2-BLL/Sign-in-reducer.ts
+++ src/features/f1-Sign-in/s2-BLL/Sign-in-reducer.ts
@@ -7,7 +7,7 @@ const initialState = {
 
 export const signInReducer = (state: StateType = initialState, action: ActionsType):StateType  => {
     switch (action.type) {
-        case "SIGN-IN/AUTH-REQUEST":
+        case "SIGN-IN/AUTH-USER":
             return {...state, user: action.user}
         default:
             return state
@@ -15,7 +15,7 @@ export const signInReducer = (state: StateType = initialState, action: ActionsTy
 }
 
 // actions
-export const authUserAC = (user: UserType) => ({type: "SIGN-IN/AUTH-REQUEST", user} as const)
+export const authUserAC = (user: UserType) => ({type: "SIGN-IN/AUTH-USER", user} as const)
 // thunks
 export const userAuthRequestTC = (loginData: LoginData) => (dispatch: Dispatch) => {
     signAPI.authRequest(loginData)
