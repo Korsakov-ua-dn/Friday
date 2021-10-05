78a2c1c0bce20b4ce5616bad7c5edde274b56c5b
korsakov <korsakov.ua.dn@gmail.com>
2021-10-05 15:08:46 +0300

    refactored headers

--- src/main/m1-UI/Header.tsx
+++ src/main/m1-UI/Header.tsx
@@ -8,13 +8,13 @@ const Header: React.FC = () => {
 
     return (
         <div className={s.header}>
-            <CustomNavlink to={Path.SIGN_IN_PATH} body={"sign-in"}/>
-            <CustomNavlink to={Path.SIGN_UP_PATH} body={"sign-up"}/>
-            <CustomNavlink to={Path.FORGOT_PATH} body={"forgot"}/>
-            <CustomNavlink to={Path.PASSWORD_ASSISTANCE_PATH} body={"assistance"}/>
+            {/*<CustomNavlink to={Path.SIGN_IN_PATH} body={"sign-in"}/>*/}
+            {/*<CustomNavlink to={Path.SIGN_UP_PATH} body={"sign-up"}/>*/}
+            {/*<CustomNavlink to={Path.FORGOT_PATH} body={"forgot"}/>*/}
+            {/*<CustomNavlink to={Path.PASSWORD_ASSISTANCE_PATH} body={"assistance"}/>*/}
             <CustomNavlink to={Path.PROFILE_PATH} body={"profile"}/>
             <CustomNavlink to={Path.LERGNOM_PACKS_LIST_PATH} body={"packs-list"}/>
-            <CustomNavlink to={Path.CARDS_LIST_PATH} body={"cards"}/>
+            {/*<CustomNavlink to={Path.CARDS_LIST_PATH} body={"cards"}/>*/}
         </div>
     );
 };
